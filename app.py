dc36066a997ae47bb739ed9ac8d98917636330ae
AlejaMontiel <167145236+AlejaMontiel@users.noreply.github.com>
2024-08-15 10:10:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ st.title("Focas bebés)
 
 st.header("En este espacio voy a hablar de las focas bebés")
 st.write("Miren ahí hay una foca bebé")
-image = Image.open
+image = Image.open('foquitas.jpg')
+
+st.write(image, caption='Una foca bebé en la naturaleza')
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
